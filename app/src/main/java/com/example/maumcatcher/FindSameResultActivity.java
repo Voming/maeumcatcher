f6964d624ea4224908e933eeca22602c97c204db
Voming <68582465+Voming@users.noreply.github.com>
2023-06-04 03:03:55 +0900

    같은 표정 찾기 최종

--- app/src/main/java/com/example/maumcatcher/FindSameResultActivity.java
+++ app/src/main/java/com/example/maumcatcher/FindSameResultActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class FindSameResultActivity extends AppCompatActivity {
     Intent intent;
@@ -16,6 +18,10 @@ public class FindSameResultActivity extends AppCompatActivity {
     Button exit;
     Button retry;
     Button note;
+
+    TextView confidence;
+    TextView extxt;
+    ImageView level;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +42,37 @@ public class FindSameResultActivity extends AppCompatActivity {
         exit = findViewById(R.id.exit);
         retry = findViewById(R.id.retry);
         note = findViewById(R.id.note);
+
+        confidence = findViewById(R.id.confidence);
+        extxt = findViewById(R.id.extxt);
+        level = findViewById(R.id.level);
+
+        if(correct == 0){
+            level.setImageResource(R.drawable.level0);
+            confidence.setText("0개를 맞췄어요");
+            extxt.setText("조금 더 노력해볼까요?");
+        }else if (correct == 1) {
+            level.setImageResource(R.drawable.level1);
+            confidence.setText("1개를 맞췄어요");
+            extxt.setText("아쉬워요");
+        }else if (correct == 2) {
+            level.setImageResource(R.drawable.level2);
+            confidence.setText("2개를 맞췄어요");
+            extxt.setText("아쉬워요");
+        }else if (correct == 3) {
+            level.setImageResource(R.drawable.level3);
+            confidence.setText("3개를 맞췄어요");
+            extxt.setText("잘했어요");
+        }else if (correct == 4) {
+            level.setImageResource(R.drawable.level4);
+            confidence.setText("4개를 맞췄어요");
+            extxt.setText("대단해요");
+        }else if (correct == 5) {
+            level.setImageResource(R.drawable.level5);
+            confidence.setText("5개를 맞췄어요");
+            extxt.setText("축하해요!");
+        }
+
 
         exit.setOnClickListener(new View.OnClickListener() {
             @Override
