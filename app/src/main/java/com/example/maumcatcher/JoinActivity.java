03f61badb4b23af00fd1f1106a6e200c8817f50a
paseu <paseue369@gmail.com>
2023-06-04 22:51:41 +0900

    로그인

---
+++ app/src/main/java/com/example/maumcatcher/JoinActivity.java
@@ -0,0 +1,93 @@
+package com.example.maumcatcher;
+
+import static com.example.login.R.*;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class JoinActivity extends AppCompatActivity {
+
+    int version = 1;
+    LoginDatabaseOpenHelper helper;
+    SQLiteDatabase database;
+
+
+    EditText idEditText;
+    EditText pwEditText;
+    EditText nameEditText;
+    EditText ageEditText;
+    Button btnJoin;
+
+    String sql;
+    Cursor cursor;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_join);
+
+        idEditText = (EditText) findViewById(id.idEditText);
+        pwEditText = (EditText) findViewById(id.pwEditText);
+        nameEditText = (EditText) findViewById(id.nameEditText);
+        ageEditText = (EditText) findViewById(id.ageEditText);
+
+        btnJoin = (Button) findViewById(id.btnJoin);
+
+        helper = new LoginDatabaseOpenHelper(JoinActivity.this, LoginDatabaseOpenHelper.tableName, null, version);
+        database = helper.getWritableDatabase();
+
+        btnJoin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                String id = idEditText.getText().toString();
+                String pw = pwEditText.getText().toString();
+                String name = nameEditText.getText().toString();
+                String age = ageEditText.getText().toString();
+
+                if(id.length() == 0 ) {
+                    //아이디와 비밀번호는 필수 입력사항입니다.
+                    Toast toast = Toast.makeText(JoinActivity.this, "아이디는 필수 입력사항입니다.", Toast.LENGTH_SHORT);
+                    toast.show();
+                    return;
+                } else if(pw.length() == 0){
+                    Toast toast = Toast.makeText(JoinActivity.this, "비밀번호는 필수 입력사항입니다.", Toast.LENGTH_SHORT);
+                    toast.show();
+                    return;
+                }else if(name.length() == 0){
+                    Toast toast = Toast.makeText(JoinActivity.this, "이름은 필수 입력사항입니다.", Toast.LENGTH_SHORT);
+                    toast.show();
+                    return;
+                }else if(age.length() == 0){
+                    Toast toast = Toast.makeText(JoinActivity.this, "나이는 필수 입력사항입니다.", Toast.LENGTH_SHORT);
+                    toast.show();
+                    return;
+                }
+
+                sql = "SELECT id FROM "+ helper.tableName + " WHERE id = '" + id + "'";
+                cursor = database.rawQuery(sql, null);
+
+                if(cursor.getCount() != 0){
+                    //존재하는 아이디입니다.
+                    Toast toast = Toast.makeText(JoinActivity.this, "존재하는 아이디입니다.", Toast.LENGTH_SHORT);
+                    toast.show();
+                }else{
+                    helper.insertUser(database,id,pw,name,age);
+                    Toast toast = Toast.makeText(JoinActivity.this, "가입이 완료되었습니다. 로그인을 해주세요.", Toast.LENGTH_SHORT);
+                    toast.show();
+                    Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/maumcatcher/LoginDatabaseOpenHelper.java
@@ -0,0 +1,45 @@
+package com.example.maumcatcher;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class LoginDatabaseOpenHelper extends SQLiteOpenHelper {
+    public static final String tableName = "Users";
+
+    public LoginDatabaseOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.i("tag","db 생성_db가 없을때만 최초로 실행함");
+        createTable(db);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVer, int newVer) {
+    }
+
+    public void createTable(SQLiteDatabase db){
+        String sql = "CREATE TABLE " + tableName + "(id text, pw text, name text, age text, avg real)";
+        db.execSQL(sql);
+    }
+
+    public void insertUser(SQLiteDatabase db, String id, String pw, String name, String age){
+        Log.i("tag","회원가입을 했을때 실행함");
+        db.beginTransaction();
+        try {
+            String sql = "INSERT INTO " + tableName + "(id, pw, name, age)" + "values('"+ id +"', '"+pw+"', '"+name+"', '"+ age+"')";
+            db.execSQL(sql);
+            db.setTransactionSuccessful();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            db.endTransaction();
+        }
+    }
+
+
+}
